7a232c8c2d6cfa9d82655b373a9f2af90959326f
Hannes Datta <h.datta@tilburguniversity.edu>
2020-12-18 21:15:07 +0100

    finalize web101

--- content/docs/tutorials/webscraping101/webscraping_101.py
+++ content/docs/tutorials/webscraping101/webscraping_101.py
@@ -2,30 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from time import sleep
-import csv 
+import csv
 
 def extract_book_urls(page_url):
     '''collect the book title and url for every book on all page urls'''
-    book_dict = {}
+    book_list = []
 
-    while page_url: 
+    while page_url:
+        print('scraping ' + page_url + '...')
         res = requests.get(page_url)
         soup = BeautifulSoup(res.text, "html.parser")
         books = soup.find_all(class_="product_pod")
 
-        for book in books: 
-            book_title = book.find("img").attrs["alt"] 
+        for book in books:
+            book_title = book.find("img").attrs["alt"]
             book_url = "https://books.toscrape.com/catalogue/" + book.find("a").attrs["href"][6:]
-            book_dict[book_title] = book_url
+            book_list.append({"title": book_title,
+                              "url": book_url})
 
         sleep(1)  # pause 1 second after each request
-        
-        if check_next_page(page_url) != None: 
+
+        if check_next_page(page_url) != None:
             page_url = "https://books.toscrape.com/catalogue/category/books_1/" + check_next_page(page_url)
-        else: 
+        else:
             break
-        
-    return book_dict
+
+    return book_list
 
 def check_next_page(url):
     res = requests.get(url)
@@ -34,22 +36,12 @@ def check_next_page(url):
     return next_btn.find("a").attrs["href"] if next_btn else None
 
 # extract title and url from all books
-book_dict = extract_book_urls("https://books.toscrape.com/catalogue/page-1.html")
+book_list = extract_book_urls("https://books.toscrape.com/catalogue/page-1.html")
 
 # save data to CSV file
-with open("book_urls.csv", "w") as csv_file: 
+with open("book_urls.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter = ";")
     writer.writerow(["title", "url", "date_time"])
     now = datetime.now()
-    for title, url in book_dict.items(): 
-        writer.writerow([title, url, now])
-
-
-
-
-
-
-
-
-
-
+    for book in book_list:
+        writer.writerow([book["title"], book["url"], now])
